4df384f09ce310b400c492080e69f5c1b5f9bc1b
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-08-17 15:05:00 +0900

    Revert "Revert "Revert "キーの入力を受け取れるように"""
    This reverts commit 92b2cf2e022649e7e714e09efa534601e44d18d5.

--- server/api/handler/controller.ts
+++
@@ -1,9 +0,0 @@
-import { tasksRepository } from '$/repository/tasksRepository';
-import { defineController } from './$relay';
-export default defineController(() => ({
-  get: () => ({ status: 200, body: 'Hello' }),
-  post: ({ body }) => ({
-    status: 200,
-    body: tasksRepository.getInputKey(body.key),
-  }),
-}));
