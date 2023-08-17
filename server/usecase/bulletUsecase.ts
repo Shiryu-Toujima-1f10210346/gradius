3571d2ab0fe2bd45d1f9e4adf38c8f33dbf157ae
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-08-17 14:54:24 +0900

    Revert "bulletUsecase"
    This reverts commit 5eb022e6918760d90833f112d49ef130c5b2b9b3.

--- server/usecase/bulletUsecase.ts
+++ server/usecase/bulletUsecase.ts
@@ -1,10 +1,8 @@
 export const bulletUsecase = {
   //xとyを保存するリスト
-  bulletCount: 0,
   bulletPosList: [] as { x: number; y: number }[],
   bulletPosSave: (x: number, y: number) => {
     bulletUsecase.bulletPosList.push({ x, y });
-    //list[n]を返す
-    return bulletUsecase.bulletPosList[bulletUsecase.bulletCount++];
+    return true;
   },
 };
